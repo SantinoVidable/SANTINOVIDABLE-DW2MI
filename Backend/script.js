0b2b0301f8f86cccf9894f16ef01b797b41cd945
Santino Vidable <vidablesantino@.com>
2024-04-29 16:44:21 -0300

    Se agregaron cosas de una ex página, y practicas sobre java script.

---
+++ Backend/script.js
@@ -0,0 +1,14 @@
+
+
+let nombreproducto= "paquetes de galletitas" 
+let preciounitario= 1000
+ 
+cantidaddeseada = prompt("Ingrese la cantidad de " + nombreproducto + " " +  "que desea comprar")
+costototal = preciounitario * cantidaddeseada
+alert("El costo total de la  compra es " + costototal)
+
+
+
+
+
+
