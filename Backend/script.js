f67f0b588858218f8c952ceee0922eea78107ca1
Santino Vidable <vidablesantino@.com>
2024-05-06 21:58:00 -0300

    se añadio un descuento del 10% al comprar 5 o mas unidades.

--- Backend/script.js
+++ Backend/script.js
@@ -2,11 +2,22 @@
 
 let nombreproducto= "paquetes de galletitas" 
 let preciounitario= 1000
+
  
 cantidaddeseada = prompt("Ingrese la cantidad de " + nombreproducto + " " +  "que desea comprar")
 costototal = preciounitario * cantidaddeseada
-alert("El costo total de la  compra es " + costototal)
 
+
+let descuento;
+if( cantidaddeseada >= 5){
+    descuento= costototal * 0.1;
+    costototalcondescuento = costototal - descuento
+    alert("El costo total de la  compra  con descuento es " + costototalcondescuento )
+}
+else{
+    descuento = 0;
+    alert("El costo total de la  compra es " + costototal)
+}
 
 
 
